76a9028df580dddbd2cce0e6834567558b0da457
bryanparmelee <bryan.parmelee@gmail.com>
2024-10-16 16:41:44 -0400

    Prepare for deployment

--- setup.py
+++ setup.py
@@ -7,11 +7,13 @@ Usage:
 
 from setuptools import setup
 
-APP = ['py-Image.py']
+APP = ['Image Resizer.py']
 DATA_FILES = ['dnd.png']
-OPTIONS = {'iconfile': '/Users/bryan-plaid/repos/py-images/ir.icns'}
+OPTIONS = {'iconfile': '/Users/bryan-plaid/repos/py-images/ir.icns', 'packages': 'setuptools'}
 
 setup(
+    name="Image Resizer",
+    version="1.0.0",
     app=APP,
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
